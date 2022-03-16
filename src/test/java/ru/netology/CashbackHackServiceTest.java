23727f6d0b2d0b36eaf104307a5a6dd6936de541
Margo4490 <95082876+Margo4490@users.noreply.github.com>
2022-03-17 00:56:51 +0300

    added JUnit5

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,61 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+
+    @Test
+    public void shouldReturn100ifPurchase900(){
+        int actual = service.remain(900);
+        int expected = 100;
+        assertEquals(expected,actual);
+
+    }
+
+    @Test
+    public void shouldReturn1ifPurchase999(){
+        int actual = service.remain(999);
+        int expected = 1;
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldReturn999ifPurchase1(){
+        int actual = service.remain(1);
+        int expected = 999;
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldReturn0ifPurchase1000(){
+        int actual = service.remain(1000);
+        int expected = 0;
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    public void shouldReturn1000ifPurchace0(){
+        int actual = service.remain(0);
+        int expected = 1000;
+        assertEquals(expected,actual);
+    }
+    @Test
+    public void shouldReturn500ifPurchace500(){
+        int actual = service.remain(500);
+        int expected = 500;
+        assertEquals(expected,actual);
+    }
+
+
+    @Test
+    public void shouldReturnMaxIfPurchaceOverMax(){
+        int actual = service.remain(2000);
+        int expected = 1000;
+        assertEquals(expected,actual);
+    }
+
+}
