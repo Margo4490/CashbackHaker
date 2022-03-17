3d0f54ae6795a2799d02ae4c0a80af15cb736f48
Margo4490 <95082876+Margo4490@users.noreply.github.com>
2022-03-17 13:36:38 +0300

    add testNG

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,9 +1,7 @@
 package ru.netology;
 
-import org.junit.Test;
-
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.*;
 
 
 class CashbackHackServiceTest {
@@ -15,7 +13,7 @@ class CashbackHackServiceTest {
     public void shouldReturn100ifPurchase900(){
         int actual = service.remain(900);
         int expected = 100;
-        assertEquals (expected,actual);
+        assertEquals (actual,expected);
 
     }
 
@@ -23,34 +21,34 @@ class CashbackHackServiceTest {
     public void shouldReturn1ifPurchase999(){
         int actual = service.remain(999);
         int expected = 1;
-        assertEquals(expected,actual);
+        assertEquals(actual,expected);
     }
 
     @Test
     public void shouldReturn999ifPurchase1(){
         int actual = service.remain(1);
         int expected = 999;
-        assertEquals(expected,actual);
+        assertEquals(actual,expected);
     }
 
     @Test
     public void shouldReturn0ifPurchase1000(){
         int actual = service.remain(1000);
         int expected = 0;
-        assertEquals(expected,actual);
+        assertEquals(actual,expected);
     }
 
     @Test
     public void shouldReturn1000ifPurchace0(){
         int actual = service.remain(0);
         int expected = 1000;
-        assertEquals(expected,actual);
+        assertEquals(actual,expected);
     }
     @Test
     public void shouldReturn500ifPurchace500(){
         int actual = service.remain(500);
         int expected = 500;
-        assertEquals(expected,actual);
+        assertEquals(actual,expected);
     }
 
 
@@ -58,7 +56,7 @@ class CashbackHackServiceTest {
     public void shouldReturnMaxIfPurchaceOverMax(){
         int actual = service.remain(2000);
         int expected = 1000;
-        assertEquals(expected,actual);
+        assertEquals(actual,expected);
     }
 
 }
